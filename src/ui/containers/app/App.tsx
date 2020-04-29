4af8f8a226afa371016e3538585e2e2dceb166cd
rafitj <rafit.jamil@gmail.com>
2020-04-29 00:12:47 -0400

    add react-router

--- src/ui/containers/app/App.tsx
+++ src/ui/containers/app/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Editor from '../../composites/Editor'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 function App() {
   return (
-    <div>
+    <Router>
+      <Switch>
+        <Route path="/notes">
+          <Editor />
+        </Route>
+        <Route path="/">
+          <h1>Home</h1>
+        </Route>
+      </Switch>
       <Editor />
-    </div>
+    </Router>
   )
 }
 
