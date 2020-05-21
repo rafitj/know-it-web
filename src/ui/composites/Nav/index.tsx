e69ee52a4946e698068ebbf538dbf1788832a9da
Shehryar Assad <xshehryar@gmail.com>
2020-05-21 02:41:45 -0400

    Add sign up form and remove default exports

--- src/ui/composites/Nav/index.tsx
+++ src/ui/composites/Nav/index.tsx
@@ -3,10 +3,11 @@ import { Box, Flex } from 'reflexbox'
 import { HighlightedText } from 'ui/components/Typography/HighlightedText'
 import { HighlightText } from 'ui/components/Typography/HighlightText'
 import { StrongText } from 'ui/components/Typography/StrongText'
+import { Link } from 'react-router-dom'
 
 export const Nav = () => {
   return (
-    <Flex alignItems="center" justifyContent="space-between" px={5} py={2}>
+    <Flex alignItems="center" justifyContent="space-between" px={5} py={2} style={{ borderBottom: '2px solid #1c1c1c' }}>
       <Box>
         <StrongText>Logo</StrongText>
       </Box>
@@ -20,6 +21,11 @@ export const Nav = () => {
           </Box>
           <Box mx={3}>
             <HighlightText highlight="green">Pricing</HighlightText>
+          </Box>
+          <Box mx={3}>
+            <Link to='/signup'>
+              <HighlightedText highlight="black">Sign Up</HighlightedText>
+            </Link>
           </Box>
           <Box mx={3}>
             <HighlightedText highlight="black">Login</HighlightedText>
