c880db9d8761662925107fae3bf0799dd5dd5168
Shehryar Assad <xshehryar@gmail.com>
2020-05-24 16:15:18 -0400

    Add some content to README and fix folder store

--- src/ui/containers/Login/LoginForm.tsx
+++ src/ui/containers/Login/LoginForm.tsx
@@ -1,48 +1,28 @@
 import React from 'react'
-import { useHistory } from 'react-router-dom'
-import { UserStore } from 'stores/UserStore'
 import { AuthForm } from 'ui/composites/AuthForm'
+import { LoginState } from './LoginState';
+import { observer } from 'mobx-react';
 
-export const LoginForm = () => {
-  const history = useHistory()
-  const [showError, setShowError] = React.useState(false)
-  const [passwordValue, setPasswordValue] = React.useState('')
-  const [emailValue, setEmailValue] = React.useState('')
-  const emailLogin = async () => {
-    const userRegisterInfo = {
-      username: emailValue,
-      password: passwordValue,
-    }
-    const loginSuccess = await UserStore.login(userRegisterInfo)
-    if (loginSuccess) {
-      history.push('/note-space')
-    } else {
-      setShowError(true)
-    }
+@observer
+export class LoginForm extends React.Component {
+  state = new LoginState();
+
+  render() {
+    return (
+      <AuthForm
+        title="Login."
+        emailText="Login with Email"
+        googleText="Login with Google"
+        fbText="Login with Facebook"
+        rerouteBtnText="Stop Procrastinating"
+        rerouteText="Don't have an account? Why?"
+        reroute="/signup"
+        setPassword={this.state.setPassword}
+        setEmail={this.state.setEmail}
+        emailClick={this.state.attemptLogin}
+        showError={this.state.showError}
+        closeError={this.state.closeError}
+      />
+    );
   }
-  const setPassword = (password: string) => {
-    setPasswordValue(password)
-  }
-  const setEmail = (email: string) => {
-    setEmailValue(email)
-  }
-  const closeError = () => {
-    setShowError(false)
-  }
-  return (
-    <AuthForm
-      title="Login."
-      emailText="Login with Email"
-      googleText="Login with Google"
-      fbText="Login with Facebook"
-      rerouteBtnText="Stop Procrastinating"
-      rerouteText="Don't have an account? Why?"
-      reroute="/signup"
-      setPassword={setPassword}
-      setEmail={setEmail}
-      emailClick={emailLogin}
-      showError={showError}
-      closeError={closeError}
-    />
-  )
 }
