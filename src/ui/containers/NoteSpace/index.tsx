cb5bdeb353cba638ef2d9da2fbe31eb771968f42
rafitj <rafit.jamil@gmail.com>
2020-05-26 02:17:41 -0400

    start func to class  refactor

--- src/ui/containers/NoteSpace/index.tsx
+++ src/ui/containers/NoteSpace/index.tsx
@@ -1,5 +1,4 @@
 import { Layout } from 'antd'
-import { observer } from 'mobx-react'
 import React from 'react'
 import { FolderStore } from 'stores/FolderStore'
 import { NoteViewStore } from 'stores/NoteViewStore'
@@ -10,38 +9,41 @@ import { NoteSideBar } from '../../composites/NoteSideBar'
 import { NoteEditor } from './NoteEditor'
 const { Content } = Layout
 
-export const NoteSpace = observer(() => {
-  const leftCollapsed = NoteViewStore.leftCollapsed
-  const rightCollapsed = NoteViewStore.rightCollapsed
-
-  React.useEffect(() => {
-    const x = async () => {
-      await FolderStore.fetchFolders()
-    }
-    x()
-  })
-
-  return (
-    <>
-      <Layout style={{ minHeight: '100vh' }}>
-        <NoteSideBar />
-        <Layout
-          style={{
-            transition: 'all 0.25s ease',
-            backgroundColor: colors.white,
-            marginLeft: leftCollapsed ? '110px' : '310px',
-            marginRight: rightCollapsed ? '110px' : '310px',
-          }}
-        >
-          <Content
-            style={{ backgroundColor: colors.white, margin: '25px 50px 0' }}
+export class NoteSpace extends React.Component {
+  leftCollapsed = NoteViewStore.leftCollapsed
+  rightCollapsed = NoteViewStore.rightCollapsed
+  fetchFolders = async () => {
+    await FolderStore.fetchFolders()
+  }
+  componentDidMount() {
+    this.fetchFolders()
+  }
+  render() {
+    return (
+      <>
+        <Layout style={{ minHeight: '100vh' }}>
+          <NoteSideBar />
+          <Layout
+            style={{
+              transition: 'all 0.25s ease',
+              backgroundColor: colors.white,
+              marginLeft: this.leftCollapsed ? '110px' : '310px',
+              marginRight: this.rightCollapsed ? '110px' : '310px',
+            }}
           >
-            <NoteEditor />
-          </Content>
+            <Content
+              style={{
+                backgroundColor: colors.white,
+                margin: '25px 50px 0',
+              }}
+            >
+              <NoteEditor />
+            </Content>
+          </Layout>
+          <ProfileBar />
+          <NoteUtilsSideBar />
         </Layout>
-        <ProfileBar />
-        <NoteUtilsSideBar />
-      </Layout>
-    </>
-  )
-})
+      </>
+    )
+  }
+}
