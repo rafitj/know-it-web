dfc925f59e8861c2d278437a911dc6713f71859e
Shehryar Assad <xshehryar@gmail.com>
2020-05-26 03:05:11 -0400

    Fix up stores

--- src/stores/FolderStore.ts
+++ src/stores/FolderStore.ts
@@ -1,47 +1,82 @@
 import { action, observable } from 'mobx'
-import { Api } from 'network/api/api'
+import { Api } from '../network/api/api';
 import CreateFolderRequest = INetwork.CreateFolderRequest;
 import GetFolderWithNotesResponse = INetwork.GetFolderWithNotesResponse;
-import FolderResponse = INetwork.FolderResponse;
 import UpdateFolderRequest = INetwork.UpdateFolderRequest;
 
-class FolderStoreImpl {
+class FolderStore {
   @observable
   folders: GetFolderWithNotesResponse[] = []
 
   @observable
   isLoading: boolean = false
 
+  @observable
+  requestError: boolean = false
+
+  @observable
+  requestErrorDetail?: string
+
   @action
   async fetchFolders() {
     this.isLoading = true
-    this.folders = await Api.getFoldersWithNotes()
+    try {
+      this.folders = await Api.fetchFoldersWithNotes();
+    } catch (e) {
+      this.requestError = true;
+      this.requestErrorDetail = 'Failed to fetch folders.';
+    }
     this.isLoading = false
   }
 
   @action
   async createFolder(
     newFolder: CreateFolderRequest
-  ): Promise<FolderResponse> {
+  ): Promise<void> {
+    this.isLoading = true
     try {
-      const res = await Api.createNewFolder(newFolder)
-      return res
-    } catch (error) {
-      throw new Error(error);
+      await Api.createFolder(newFolder);
+    } catch (e) {
+      this.requestError = true;
+      this.requestErrorDetail = 'Failed to create folder.';
     }
+    this.isLoading = false
   }
 
   @action
   async updateFolder(folder: UpdateFolderRequest) {
-    await Api.updateFolder(folder)
-    await this.fetchFolders()
+    this.isLoading = true
+    try {
+      await Api.updateFolder(folder)
+      await this.fetchFolders()
+    } catch (e) {
+      this.requestError = true;
+      this.requestErrorDetail = 'Failed to update folder.';
+    }
+    this.isLoading = false
   }
 
   @action
   async deleteFolder(folderId: string) {
-    await Api.deleteFolder(folderId)
-    await this.fetchFolders()
+    this.isLoading = true
+    try {
+      await Api.deleteFolder(folderId)
+      await this.fetchFolders()
+    } catch (e) {
+      this.requestError = true;
+      this.requestErrorDetail = 'Failed to delete folder.';
+    }
+    this.isLoading = false
+  }
+
+  @action
+  resetErrors(): void {
+    this.requestError = false;
+    this.requestErrorDetail = undefined;
   }
 }
 
-export const FolderStore = new FolderStoreImpl()
+const folderStore = new FolderStore();
+export {
+  folderStore as FolderStore
+}
