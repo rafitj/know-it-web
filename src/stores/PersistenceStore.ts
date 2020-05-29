2d0fbe0a8147278968d04cb1b87d920e12b38995
Rafit Jamil <rafit.jamil@gmail.com>
2020-05-29 03:08:26 -0400

    recent notes persistence (#7)
    * stuff
    
    * type

--- src/stores/PersistenceStore.ts
+++ src/stores/PersistenceStore.ts
@@ -1,8 +1,11 @@
 import { action, observable } from 'mobx'
+import { RecentNote } from '../network/proto/protos'
+import { NoteState } from './NoteStore'
 import { UserStore } from './UserStore'
 
 export enum PersistenceKey {
   UserSession = 'UserSession',
+  RecentNotes = 'RecentNotes',
 }
 
 type AppLoadRoutineType = () => void
@@ -17,6 +20,12 @@ const appLoadRoutine: AppLoadRoutineType[] = [
     UserStore.authToken = token
     UserStore.fetchUser(token)
   },
+  async () => {
+    const storageValue = await localStorage.getItem(PersistenceKey.RecentNotes)
+    const recentFiles =
+      storageValue === null ? [] : (JSON.parse(storageValue) as RecentNote[])
+    await NoteState.setRecentNoteIds(recentFiles)
+  },
 ]
 
 class PersistenceStore {
@@ -30,6 +39,52 @@ class PersistenceStore {
     this.isLoading = false
   }
 
+  @action
+  storeRecentNotes = async (note: RecentNote) => {
+    const storageValue = await localStorage.getItem(PersistenceKey.RecentNotes)
+    const recentFiles =
+      storageValue === null ? [] : (JSON.parse(storageValue) as RecentNote[])
+    if (!recentFiles.find((file) => file.id === note.id)) {
+      recentFiles.push(note)
+      if (recentFiles.length > 3) {
+        recentFiles.shift()
+      }
+      localStorage.setItem(
+        PersistenceKey.RecentNotes,
+        JSON.stringify(recentFiles)
+      )
+    }
+  }
+
+  @action
+  deleteRecentNotesStorage = async (noteId: string) => {
+    const storageValue = localStorage.getItem(PersistenceKey.RecentNotes)
+    if (storageValue !== null) {
+      const recentFiles = JSON.parse(storageValue) as RecentNote[]
+      const updatedFiles = recentFiles.filter(({ id }) => id !== noteId)
+      localStorage.setItem(
+        PersistenceKey.RecentNotes,
+        JSON.stringify(updatedFiles)
+      )
+    }
+  }
+
+  @action
+  updateRecentNotesStorage = async (note: RecentNote) => {
+    const storageValue = localStorage.getItem(PersistenceKey.RecentNotes)
+    if (storageValue !== null) {
+      const recentFiles = JSON.parse(storageValue) as RecentNote[]
+      const updatedFiles = recentFiles.map((storageNote) =>
+        storageNote.id === note.id
+          ? { id: note.id, title: note.title }
+          : storageNote
+      )
+      localStorage.setItem(
+        PersistenceKey.RecentNotes,
+        JSON.stringify(updatedFiles)
+      )
+    }
+  }
   setItem<T>(key: PersistenceKey, data: string) {
     localStorage.setItem(key.valueOf(), data)
   }
