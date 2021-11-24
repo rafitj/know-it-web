39e87912a9ab843a96af7e8612b092a16c564fd5
Hussain Abdi <hussainabdi12@gmail.com>
2021-11-24 07:58:48 -0500

    update storybook button

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,17 @@
 module.exports = {
-  stories: ['../src/**/*.stories.tsx'],
+  stories: ['../src/**/*.stories.mdx'],
   addons: [
+    '@storybook/preset-typescript',
     '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
     '@storybook/addon-links',
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        configureJSX: true,
+        babelOptions: {},
+        sourceLoaderOptions: null,
+      },
+    },
   ],
-};
+}
