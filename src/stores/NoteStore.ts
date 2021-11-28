9195b314cc72e1808f7f0590a9d460794b5e3707
Rafit Jamil <rafit.jamil@gmail.com>
2021-11-27 23:59:46 -0500

    Fix up stores

--- src/stores/NoteStore.ts
+++ src/stores/NoteStore.ts
@@ -5,43 +5,79 @@ import NoteResponse = INetwork.NoteResponse;
 import CreateNoteRequest = INetwork.CreateNoteRequest;
 import UpdateNoteRequest = INetwork.UpdateNoteRequest;
 
-class NoteStoreImpl {
+class NoteStore {
   @observable
-  note?: NoteResponse = undefined
+  note?: NoteResponse;
 
   @observable
   isLoading: boolean = false
 
+  @observable
+  requestError: boolean = false
+
+  @observable
+  requestErrorDetail?: string
+
   @action
   async fetchNote(id: string) {
     this.isLoading = true
-    this.note = await Api.getNoteById(id)
+    try {
+      this.note = await Api.fetchNote(id)
+    } catch (e) {
+      this.requestError = true;
+      this.requestErrorDetail = 'Failed to fetch note.';
+    }
     this.isLoading = false
   }
 
   @action
   async createNote(payload: CreateNoteRequest) {
     this.isLoading = true
-    this.note = await Api.createNewNote(payload)
-    await FolderStore.fetchFolders()
+    try {
+      this.note = await Api.createNote(payload)
+      await FolderStore.fetchFolders()
+    } catch (e) {
+      this.requestError = true;
+      this.requestErrorDetail = 'Failed to create note.';
+    }
     this.isLoading = false
   }
 
   @action
   async updateNoteById(payload: UpdateNoteRequest) {
     this.isLoading = true
-    this.note = await Api.updateNoteById(payload)
-    // await FolderStore.fetchFolders()
+    try {
+      this.note = await Api.updateNote(payload)
+      await FolderStore.fetchFolders()
+    } catch (e) {
+      this.requestError = true;
+      this.requestErrorDetail = 'Failed to update note.';
+    }
     this.isLoading = false
   }
 
   @action
   async deleteNoteById(id: string) {
     this.isLoading = true
-    await Api.deleteNoteById(id)
-    await FolderStore.fetchFolders()
+    try {
+      await Api.deleteNoteById(id)
+      await FolderStore.fetchFolders()
+    } catch (e) {
+      this.requestError = true;
+      this.requestErrorDetail = 'Failed to delete note.';
+    }
     this.isLoading = false
+  }
+
+  @action
+  resetErrors(): void {
+    this.requestError = false;
+    this.requestErrorDetail = undefined;
   }
 }
 
-export const NoteStore = new NoteStoreImpl()
+const noteStore = new NoteStore();
+
+export {
+  noteStore as NoteStore
+}
